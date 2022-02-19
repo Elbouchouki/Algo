13a14e9985b81b658025573348f652e7a8f1ccd1
Elbouchouki <elbouchoukigamer@gmail.com>
2022-02-19 17:52:13 +0100

    sparse array

---
+++ comp-problems/sparse-Array/sparse-array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> matchingStrings(vector<string> strings, vector<string> queries)
+{
+}
+int main()
+{
+    vector<string> strings = {};
+    vector<string> queries = {};
+}
