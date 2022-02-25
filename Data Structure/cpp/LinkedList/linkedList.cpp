42bf105dee71a178c5a9cf60aae793a5bcea6517
Elbouchouki <elbouchoukigamer@gmail.com>
2022-02-25 22:55:30 +0100

    comportement FILE

---
+++ Data Structure/cpp/LinkedList/linkedList.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+/* ----------------------------- structure noeud ---------------------------- */
+template <class g_type> // type generique pour les noeuds (generique = variable)
+struct Node
+{
+    // attribues
+    g_type value;
+    Node *next;
+    // initialiser un noeud (creation) a vide
+    Node()
+    {
+        value = 0;
+        next = nullptr;
+    }
+    // initialiser un noeud (creation) avec des arguments
+    Node(g_type v, Node *n)
+    {
+        value = v;
+        next = n;
+    }
+};
+
+/* ------------------- structure list simplement chainnee ------------------- */
+template <class g_type>
+struct LinkedList
+{
+    // attribues
+    struct Node<g_type> *head;
+    struct Node<g_type> *tail;
+    size_t length;
+    // initialiser la list (creation)
+    LinkedList()
+    {
+        head = tail = nullptr;
+        length = 0;
+    }
+    // afficher la list
+    void display()
+    {
+        if (length == 0)
+        {
+            cout << "Empty." << endl;
+            return;
+        }
+        struct Node<g_type> *itr_node = head;
+        while (itr_node != nullptr)
+        {
+            cout << itr_node->value << " ";
+            itr_node = itr_node->next;
+        }
+        cout << endl;
+        delete (itr_node);
+    }
+    /* ---------------------------- comportement FILE --------------------------- */
+    void enqueue(g_type value)
+    {
+        struct Node<g_type> *temp_node = new Node<g_type>(value, head);
+        head = temp_node;
+        length++;
+    }
+    g_type dequeue()
+    {
+        if (length == 0)
+        {
+            cout << "Linkedlist is empty. ";
+            return 0;
+        }
+        g_type v = head->value;                // conserver la valeur de l'element a supprimer.
+        struct Node<g_type> *temp_node = head; // conserver le noeud a supprimer pour le supprimer de la memoire.
+        head = head->next;
+        delete (temp_node);
+        length--;
+        return v;
+    }
+    /* ----------------------------------- end ---------------------------------- */
+
+    /* ---------------------------- comportement PILE --------------------------- */
+    void push(g_type value)
+    {
+    }
+    g_type top()
+    {
+    }
+    g_type pop()
+    {
+    }
+    /* ----------------------------------- end ---------------------------------- */
+};
+
+// programme principale
+int main()
+{
+    LinkedList<int> list;
+    list.display();
+    list.enqueue(1);
+    list.enqueue(2);
+    list.display();
+    cout << "dequeued" << list.dequeue() << endl;
+    list.display();
+    list.dequeue();
+    list.display();
+    list.dequeue();
+
+    return 0;
+}
