f5547af2ff9d6e92f9f1a2f2b70ae6221c73bb98
Elbouchouki <elbouchoukigamer@gmail.com>
2022-04-03 01:25:55 +0000

    2 usm

---
+++ problem solving/leetcode/DS/Two_Sum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    static vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m;
+        for(int i=0 ; i<nums.size() ; i++){
+            if( m.find(target - nums[i]) != m.end() ){
+                return vector<int>({m[target - nums[i]],i});
+            }
+            m.insert({nums[i],i});
+        }
+        return vector<int>();
+    }
+};
+
+int main(){
+    vector<int> nums ({2,7,11,15});
+    int target(9);
+    vector<int> result = Solution::twoSum(nums,target);
+    cout << result[0] << " " << result[1] <<endl;
+    nums = {3,2,4};
+    target = 6;
+    result = Solution::twoSum(nums,target);
+    cout << result[0] << " " << result[1] <<endl;
+    nums = {3,3};
+    target = 6;
+    result = Solution::twoSum(nums,target);
+    cout << result[0] << " " << result[1] <<endl;
+}
